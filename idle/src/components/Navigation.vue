10e37677aee5302072344c4012316837c563b872
Vin Myca <vinmycasagarino@gmail.com>
2020-12-13 23:59:15 +0800

    v: added side navigation

---
+++ idle/src/components/Navigation.vue
@@ -0,0 +1,156 @@
+<template>
+  <v-navigation-drawer color="rgb(20, 118, 210)" permanent app>
+    <v-list-item>
+      <v-list-item-content class="mt-2 ml-2">
+        <v-list-item-title class="title white--text"> IDLE </v-list-item-title>
+        <v-list-item-subtitle></v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+    <v-divider></v-divider>
+
+    <v-list dense nav>
+      <v-list-item-group color="blue">
+        <v-list-item class="white--text">
+          <v-list-item-icon class="ma-0 mt-2 mr-2">
+            <v-icon small style="color: #d1e7f9">mdi-view-dashboard</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <router-link to="/dashboard" style="color: #d1e7f9"
+              >Dashboard</router-link
+            >
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item class="white--text">
+          <v-list-item-icon class="ma-0 mt-2 mr-2">
+            <v-icon small style="color: #d1e7f9">mdi-human-queue</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <router-link to="/queue" style="color: #d1e7f9"
+              >Queue</router-link
+            ></v-list-item-content
+          >
+        </v-list-item>
+
+        <v-list-group color="white">
+          <template v-slot:activator>
+            <v-list-item-icon class="ma-0 mt-2 mr-2">
+              <v-icon small style="color: #d1e7f9">mdi-source-branch</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content style="color: #d1e7f9">
+              Manage Branch
+            </v-list-item-content>
+          </template>
+          <v-list-item>
+            <v-list-item-content class="ml-10 pl-4 list-item">
+              <router-link to="/branch/add" style="color: #a4cff4"
+                >Add Branch</router-link
+              >
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content class="ml-10 pl-4 list-item">
+              <router-link to="/branch/edit" style="color: #a4cff4"
+                >Edit Branch</router-link
+              >
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content class="ml-10 pl-4 list-item">
+              <router-link to="/branch/delete" style="color: #a4cff4"
+                >Delete Branch</router-link
+              >
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-group>
+
+        <v-list-group color="white">
+          <template v-slot:activator>
+            <v-list-item-icon class="ma-0 mt-2 mr-2">
+              <v-icon small style="color: #d1e7f9">mdi-account-group</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content style="color: #d1e7f9">
+              Manage Account
+            </v-list-item-content>
+          </template>
+          <v-list-item>
+            <v-list-item-content class="ml-10 pl-4 list-item">
+              <router-link to="/account/add" style="color: #a4cff4"
+                >Add Account</router-link
+              >
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content class="ml-10 pl-4 list-item">
+              <router-link to="/account/edit" style="color: #a4cff4"
+                >Edit Account</router-link
+              >
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content class="ml-10 pl-4 list-item">
+              <router-link to="/account/delete" style="color: #a4cff4"
+                >Delete Account</router-link
+              >
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-group>
+
+        <v-list-group color="white">
+          <template v-slot:activator>
+            <v-list-item-icon class="ma-0 mt-2 mr-2">
+              <v-icon small style="color: #d1e7f9">mdi-domain</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content style="color: #d1e7f9">
+              Manage Business
+            </v-list-item-content>
+          </template>
+          <v-list-item>
+            <v-list-item-content class="ml-10 pl-4 list-item">
+              <router-link to="/business/add" style="color: #a4cff4"
+                >Add Business</router-link
+              >
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content class="ml-10 pl-4 list-item">
+              <router-link to="/business/edit" style="color: #a4cff4"
+                >Edit Business</router-link
+              >
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content class="ml-10 pl-4 list-item">
+              <router-link to="/business/delete" style="color: #a4cff4"
+                >Delete Business</router-link
+              >
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-group>
+      </v-list-item-group>
+    </v-list>
+    <template v-slot:append>
+      <div class="pa-2 ma-2 d-flex flex-row-reverse">
+        <v-btn depressed small rounded color="primary">
+          Logout
+          <v-icon right small> mdi-logout </v-icon>
+        </v-btn>
+      </div>
+    </template>
+  </v-navigation-drawer>
+</template>
+<style scoped>
+.list-item {
+  font-size: 14px;
+}
+</style>
+<script>
+export default {
+  name: "AdminNavBar",
+  data() {
+    return {
+      drawer: null,
+    };
+  },
+};
+</script>
