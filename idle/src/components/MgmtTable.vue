b9c9ff803a4eec7ecb2358d227b32d9185089199
Vin Myca <vinmycasagarino@gmail.com>
2020-12-20 05:17:47 +0800

    v: editted business mgmt

--- idle/src/components/MgmtTable.vue
+++ idle/src/components/MgmtTable.vue
@@ -1,7 +1,15 @@
 <template>
   <v-container>
     <div class="my-6">
-    
+      <v-text-field
+        v-model="search"
+        label="Search"
+        class="mx-4"
+        prepend-inner-icon="mdi-magnify"
+        outlined
+        dense
+        hide-details
+      ></v-text-field>
     </div>
     <template>
       <div>
@@ -28,7 +36,7 @@
               v-on:click="onButtonClick(props.item)"
               class="white--text ml-3 mr-7"
             >
-              DELETE <v-icon small>mdi-pencil</v-icon>
+              DELETE <v-icon small>mdi-delete-outline</v-icon>
             </v-btn>
           </template>
         </v-data-table>
