48cea55a6b00a8931a460b761a9d0290d6187a80
Vin Myca <vinmycasagarino@gmail.com>
2020-12-20 18:04:15 +0800

    v: editted form for add account

--- idle/src/views/ManageAccount.vue
+++ idle/src/views/ManageAccount.vue
@@ -20,11 +20,72 @@
                 outlined
                 dense
                 required
+                prepend-icon="mdi-account"
+                class="mb-5"
+              ></v-text-field>
+              <v-select
+                :items="branches"
+                label="Branch*"
+                outlined
+                hide-details
+                class="mb-2"
+                prepend-icon="mdi-source-branch"
+                dense
+              ></v-select>
+              <v-row>
+                <v-col sm="5">
+                  <v-select
+                    :items="branches"
+                    label="Month*"
+                    outlined
+                    hide-details
+                    class="mb-2"
+                    dense
+                    prepend-icon="mdi-cake-variant"
+                  ></v-select>
+                </v-col>
+                <v-col sm="3">
+                  <v-select
+                    :items="branches"
+                    label="Day*"
+                    outlined
+                    hide-details
+                    class="mb-2"
+                    dense
+                  ></v-select>
+                </v-col>
+                <v-col sm="4">
+                  <v-select
+                    :items="branches"
+                    label="Year*"
+                    outlined
+                    hide-details
+                    class="mb-2"
+                    dense
+                  ></v-select>
+                </v-col>
+              </v-row>
+
+              <v-text-field
+                label="Email*"
+                hide-details
+                outlined
+                dense
+                required
+                prepend-icon="mdi-email"
+                class="mb-5"
+              ></v-text-field>
+              <v-text-field
+                label="Password*"
+                hide-details
+                outlined
+                dense
+                prepend-icon="mdi-lock"
+                required
               ></v-text-field>
             </v-container>
-            <small class="ml-5">*indicates required field</small>
           </v-card-text>
-          <v-card-actions class="pr-8 pb-8">
+          <v-card-actions class="pr-8 pt-5 pb-8">
             <v-spacer></v-spacer>
             <v-btn rounded class="px-8" v-on:click="add = false">
               Cancel
@@ -42,44 +103,110 @@
       </v-dialog>
 
       <v-dialog v-model="edit" persistent max-width="600px" slot="editBtn">
-              <template v-slot:activator="{ on, attrs }">
-            <v-btn
-              color="amber darken-2"
-              small
-              v-bind="attrs"
-                v-on="on"
-              class="white--text"
-            >
-              EDIT <v-icon small>mdi-pencil</v-icon>
-            </v-btn>
-             </template>
-            <v-card>
-              <v-card-title>
-                <span class="headline pl-3 pt-3">Edit Account</span>
-              </v-card-title>
-              <v-card-text class="pb-0">
-                <v-container>
-                  <v-text-field
-                    label="Name*"
-                    hide-details
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            color="amber darken-2"
+            small
+            v-bind="attrs"
+            v-on="on"
+            class="white--text"
+          >
+            EDIT <v-icon small>mdi-pencil</v-icon>
+          </v-btn>
+        </template>
+        <v-card>
+          <v-card-title>
+            <span class="headline pl-3 pt-3">Edit Account</span>
+          </v-card-title>
+          <v-card-text class="pb-0">
+            <v-container>
+              <v-text-field
+                label="Name*"
+                hide-details
+                outlined
+                dense
+                required
+                prepend-icon="mdi-account"
+                class="mb-5"
+              ></v-text-field>
+              <v-select
+                :items="branches"
+                label="Branch*"
+                outlined
+                hide-details
+                class="mb-2"
+                prepend-icon="mdi-source-branch"
+                dense
+              ></v-select>
+              <v-row>
+                <v-col sm="5">
+                  <v-select
+                    :items="branches"
+                    label="Month*"
                     outlined
+                    hide-details
+                    class="mb-2"
                     dense
-                    required
-                  ></v-text-field>
-                </v-container>
-                <small class="ml-5">*indicates required field</small>
-              </v-card-text>
-              <v-card-actions class="pr-8 pb-8">
-                <v-spacer></v-spacer>
-                <v-btn rounded class="px-8" v-on:click="edit = false">
-                  Cancel
-                </v-btn>
-                <v-btn color="primary" class="px-10" rounded v-on:click="edit = false">
-                  Save
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
+                    prepend-icon="mdi-cake-variant"
+                  ></v-select>
+                </v-col>
+                <v-col sm="3">
+                  <v-select
+                    :items="branches"
+                    label="Day*"
+                    outlined
+                    hide-details
+                    class="mb-2"
+                    dense
+                  ></v-select>
+                </v-col>
+                <v-col sm="4">
+                  <v-select
+                    :items="branches"
+                    label="Year*"
+                    outlined
+                    hide-details
+                    class="mb-2"
+                    dense
+                  ></v-select>
+                </v-col>
+              </v-row>
+
+              <v-text-field
+                label="Email*"
+                hide-details
+                outlined
+                dense
+                required
+                prepend-icon="mdi-email"
+                class="mb-5"
+              ></v-text-field>
+              <v-text-field
+                label="Password*"
+                hide-details
+                outlined
+                dense
+                prepend-icon="mdi-lock"
+                required
+              ></v-text-field>
+            </v-container>
+          </v-card-text>
+          <v-card-actions class="pr-8 pb-8">
+            <v-spacer></v-spacer>
+            <v-btn rounded class="px-8" v-on:click="edit = false">
+              Cancel
+            </v-btn>
+            <v-btn
+              color="primary"
+              class="px-10"
+              rounded
+              v-on:click="edit = false"
+            >
+              Save
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </MgmtTable>
   </v-container>
 </template>
@@ -101,17 +228,19 @@ export default {
             {
               id: "0001",
               name: "Sample",
-              roleName: "Dummy",
+              branch: "Dummy",
               birthdate: "12/25/2020",
               email: "sample@gmail.com",
+              password: "a131jgjdsasjhda8asbfa",
               createdAt: "12/15/2020",
             },
             {
               id: "0002",
               name: "Chongyun",
-              roleName: "Customer",
+              branch: "Liyue",
               birthdate: "09/07/2020",
               email: "chonghyun@gmail.com",
+              password: "asghdj3hdas8b32l1asld",
               createdAt: "12/15/2020",
             },
           ],
@@ -122,14 +251,16 @@ export default {
               value: "id",
             },
             { text: "Name", value: "name" },
-            { text: "Role", value: "roleName" },
+            { text: "Branch", value: "branch" },
             { text: "Birthday", value: "birthdate" },
             { text: "Email", value: "email" },
+            { text: "Password", value: "password" },
             { text: "Date Created", value: "createdAt" },
             { text: "", value: "controls", sortable: false, align: "end" },
           ],
         },
       ],
+      branches: ["Hi", "Hello"],
     };
   },
 };
